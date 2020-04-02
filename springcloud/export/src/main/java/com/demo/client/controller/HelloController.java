3768c74995133dfea7c5b2190c6c1de7f1ceb219
yin.weiwei <569800752@qq.com>
2020-04-02 19:48:42 +0800

    update spring-cloud

--- springcloud/export/src/main/java/com/demo/client/controller/HelloController.java
+++ springcloud/export/src/main/java/com/demo/client/controller/HelloController.java
@@ -1,16 +1,27 @@
 package com.demo.client.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class HelloController {
 	@Value("${server.port}")
 	private int port;
 
+	@Value("${test.str}")
+	private String str;
+
 	@RequestMapping("index")
 	public String index(){
 		return "Hello World!,端口："+port;
 	}
+	@RequestMapping("name")
+	public String name(){
+		return "Hello World!,name："+str;
+	}
 }
+
+
--- springcloud/gateway/src/main/java/com/demo/gateway/App.java
+++ springcloud/gateway/src/main/java/com/demo/gateway/App.java
@@ -2,8 +2,10 @@ package com.demo.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringCloudApplication
+@EnableZuulProxy
 public class App {
 
 	public static void main(String[] args) {
